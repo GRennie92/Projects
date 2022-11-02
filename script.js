d7d35b2166c7995d14907e43bb8ce41cbe31da40
Graeme Rennie <ec1521160@edinburghcollege.ac.uk>
2022-11-02 21:20:11 +0000

    Started editing the html file and building js file

---
+++ script.js
@@ -0,0 +1,36 @@
+const calculator ={
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null
+}
+
+function updateResult(){
+    const result = document.querySelector('.result');
+    result.value = calculator.displayValue;
+}
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) =>{
+    const { target } = event;
+    if(!target.matches('button')){
+        return;
+    }
+
+    if(target.classList.contains('operator')){
+        console.log('operator', target.value);
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        console.log('decimal', target.value);
+        return;
+      }
+    
+      if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+      }
+    
+      console.log('digit', target.value);
+    });
